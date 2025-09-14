100394e9d227d269c1045931b3a4536272408aec
VuXfi <x@42.pub>
2025-09-14 20:18:35 +0700

    Fix export statement for AppAdminizer to include default export

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from "./src/utils/decorators";
 export * from "./src/types";
 export * from "adminizer";
-export * from "./src/AppAdminizer"
+export * from "./src/AppAdminizer";
+export { AppAdminizer as default } from "./src/AppAdminizer";
