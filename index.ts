ad48895beeb56df5f3bb05a057250d06e512f698
VuXfi <x@42.pub>
2025-09-14 19:45:46 +0700

    Add newline and reorder exports in index.ts for clarity

--- index.ts
+++ index.ts
@@ -2,3 +2,6 @@ export * from "./src/utils/decorators";
 export * from "./src/types";
 export * from "adminizer";
 export * from "./src/AppAdminizer"
+
+// Default export
+export { default } from "./src/AppAdminizer";
